788b3b00d05bd91b41459a8da303b6177ed0a6e4
mannavad <82090110+mannavad@users.noreply.github.com>
2024-11-04 23:04:17 +0530

    Create jfrog.groovy

---
+++ vars/jfrog.groovy
@@ -0,0 +1,20 @@
+def rtUpload (
+    serverId: 'jfrog,
+    spec: '''{
+          "files": [
+            {
+              "pattern": "*.war",
+              "target": "libs-snapshot-local/"
+            }
+         ]
+    }''',
+
+    // Optional - Associate the uploaded files with the following custom build name and build number,
+    // as build artifacts.
+    // If not set, the files will be associated with the default build name and build number (i.e the 
+    // the Jenkins job name and number).
+    buildName: 'holyFrog',
+    buildNumber: '42',
+    // Optional - Only if this build is associated with a project in Artifactory, set the project key as follows.
+    project: 'my-project-key'
+)
